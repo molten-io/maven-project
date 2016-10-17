fe14c2bd7a693a5cfbc2d6c53205f79adfbb6961
mosten-io <molten-io@test.com>
2016-10-17 14:39:13 +1100

    Adding failing test

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,6 +1,9 @@
 package com.mulesoft.training;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
@@ -13,13 +16,13 @@ public class HelloMavenTest extends FunctionalTestCase {
     
     // retrieveFlights response contains content-type header
     // content-type headers has a value of application/json
- /*   @Test
+   @Test
     public void retrieveFlightsAddsAppropriateHeader() throws Exception{
     	MuleEvent event = runFlow("retrieveFlights");
     	String contentType = event.getMessage().getOutboundProperty("Content-Type");
     	assertEquals("application/json", contentType);
     }
-   */
+
     // run maven-config.xml when testing
     @Override
     protected String getConfigFile() {
